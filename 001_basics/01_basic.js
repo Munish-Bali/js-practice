3b5daee7dea458e2c84729e42f0ff4c2c238c3a6
Munish Bali <78945518+Munish-Bali@users.noreply.github.com>
2025-08-05 13:34:34 +0000

    Basic and DataTypes

---
+++ 001_basics/01_basic.js
@@ -0,0 +1,56 @@
+console.log("Welcome to Munish's new Venture");
+
+const accountId= 14453
+let accountEmail="munish@gmail.com"
+var accountPassword = "Munsih@123"
+accountCity="Jammu"
+
+// accountId = 2
+
+
+// console.log(accountId)
+
+accountEmail="munish123@gmail.com"
+
+accountPassword="Mic@123"
+accountCity="Jammu and Kashmir"
+
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+let accountState;
+
+console.table[accountId, accountPassword, accountCity, accountEmail]
+console.table([accountId, accountPassword, accountEmail, accountCity, accountState])
+
+/*Prefer not to use var , because of the scope issue*/
+
+/*DATA Types In JS*/
+
+"use strict";// treat all js code as new version.
+
+// alert("hello"); // we are using node js not browser. there is a different way to use alert in node js. But you can use this way to call alert in browser.
+
+console.log(3+4); //code readability should be high.
+
+let name ="MUnish";
+let age=28;
+let isLogIN =  false;
+
+
+//number => 2 to power 53
+//bigInt =
+//String => ""
+//boolean =>true/false
+//null => standalone value
+// undefinded =>
+//syymbol => unique
+
+//Object
+
+console.log(typeof "munish");
+
+console.log(typeof null); // type Of Null is Object
+console.log(typeof undefined);  // type of Undefined is Undefined.
+
